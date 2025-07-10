14a53a4dae89b67536a3dfab5c5c742bbf42b3db
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2025-07-10 05:11:45 +0300

    fake commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_project/doc_app.dart';
+
+import 'core/routing/app_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Advanced',
-      theme: ThemeData(
-
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-
-    );
-  }
-}
-
